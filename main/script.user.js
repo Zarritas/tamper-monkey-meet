c2c982ebb4f8af98e0ab0d2c855becee6282fcae
Jesús Lorenzo Limón <132434631+Zarritas@users.noreply.github.com>
2025-11-23 12:24:33 +0100

    Update description assignment to use querySelector

--- main/script.user.js
+++ main/script.user.js
@@ -80,14 +80,14 @@
         if (!await ensureAuth()) return
         is_daily = true
         await setProjectAndTask("Temas internos", "Daily")
-        document.getElementById('description').value = document.getElementById('ucc-7').textContent
+        document.getElementById('description').value = document.querySelector('NeC6gb').textContent
     }
 
     async function setRefinementReport(){
         if (!await ensureAuth()) return
         is_daily = false
         await setProjectAndTask("Temas internos", "Refinement")
-        document.getElementById('description').value = document.getElementById('ucc-7').textContent.replace('Daily', 'Refinamiento')
+        document.getElementById('description').value = document.querySelector('NeC6gb').textContent.replace('Daily', 'Refinamiento')
     }
 
     async function setStaticUrlReport(element){
